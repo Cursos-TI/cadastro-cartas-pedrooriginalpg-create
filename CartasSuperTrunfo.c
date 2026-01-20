ad6692bdec3ee805e5256ff469e9cb00d98f6678
pedrooriginalpg-create <pedro.originalpg@gmail.com>
2026-01-20 19:32:33 -0300

    Add comments for clarity in CartasSuperTrunfo.c
    comentrando a populacao da cidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,17 +1,55 @@
-#include <stdio.h>
+#include <stdio.h> 
+  /*
+  Biblioteca enteda e saida da linguaguem em C e sem ela não poderiamos usar printf ou scanf
+  */
 
-int main(){
+int main()
+{ 
+   /*
+   A Função principal do programa e a Área para colocarmos as variaveis.
+   */
 
-//Área  para definição das variaveis para armazenar as propriedades das cidades 
-  char cidade [50];
+  //Área  para definição das variaveis para armazenar as propriedades das cidades 
+
+  char cidade [50]; 
+    /*
+    Armazena o nome da cidade e seus nomes.
+    */
+  
   int populacao;
+    /*
+    Variável do tipo inteiro,armazena a população total da cidade em número  de habitantes.
+    */
+  
   float area;
+    /*
+    Variável  do tipo float que armazena a aárea  total da cidade em km² e pode conter valores decimais.
+    */
+  
   float pib;
+    /*
+    Variável  do tipo float que armazena o PIB  total da cidade em bilhões de reais  e pode conter valores decimais.
+    */
+  
   int pontos turisticos;
-
+    /*
+    Variável do tipo inteiro que  armazena os pontos turisticos das cidades.
+    */
+ 
+  
   //Área  para entrada de dados 
-  printf("digite o nome da cidade: ");
+  
+    printf("digite o nome da cidade: ");
+      /*
+      imprime uma imagwm na tela e pede par que seja digitado o nome da cidade.
+      */
+  
   scanf(" %[^\n]", cidade);
+        /*
+        scanf lê o texto digitado e o " %[^\n]" permite ler frases com espaços,
+        parando apenas quando o usuário apertar ENTER e o valor digitado será armazenado na variável cidade.
+.
+        */
 
   printf("digite a populacao da cidade: ");
   scanf("%d", &populacao);
