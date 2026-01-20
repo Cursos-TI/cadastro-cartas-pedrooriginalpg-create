d46a7b0bb187a88c301f83cd6069d62509578c55
pedrooriginalpg-create <pedro.originalpg@gmail.com>
2026-01-19 22:43:14 -0300

    Refactor city data input and output in CartasSuperTrunfo
    Fixed input/output formatting and variable assignments for city properties.adicionei o codigo completoi com ajuda de IA

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,18 +1,37 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+int main(){
 
-int main() {
-  printf("desafio super trunfo"\n);
-  printf("novo comit\n");
+//Área  para definição das variaveis para armazenar as propriedades das cidades 
+  char cidade [50];
+  int populacao;
+  float area;
+  float pib;
+  int pontos turisticos;
+
+  //Área  para entrada de dados 
+  printf("digite o nome da cidade: ");
+  scanf(" %[^\n]", cidade);
+
+  printf("digite a populacao da cidade: ");
+  scanf("%d", &populacao);
+
+  printf("digite a area da cidade (km²): ");
+  scanf("%f", &area);
   
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+  printf("digite o PIB da cidade (em bilhoes): ");
+  scanf("%f",&pontosturisticos: );
 
-  // Área para entrada de dados
+   printf("Digite o numero de pontos turisticos");
+  scanf("%d, &pontosturisticos: ");
 
-  // Área para exibição dos dados da cidade
+  //Área para exibicao dos dados da cidade
+printf("\n===== carta super trunfo=====\nn");
+  printf("cidade: %s\n, cidade");
+printf("populacao: %d habitantes\n, populacao");
+  printf("Area: %.2f kn²\n",area);
+  printf("PIB: %2.f bilhoes\n", pib);
+  prinf("pontosd turisticos: %d\n, pontosturisticos");
 
-return 0;
-} 
+  return 0;
+}
